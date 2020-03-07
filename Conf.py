408285045aac2e3091fb84d30976c891b46ae824
Dusty Williams <dusty.wil@gmail.com>
2020-03-07 14:25:46 -0500

    css files, initial login page - dmw

--- Conf.py
+++ Conf.py
@@ -1,5 +1,11 @@
 import os
+base_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Conf:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "h0 ly h4n dgr 3n4d 3!"
+    SECRET_KEY = os.environ.get("SECRET_KEY") \
+        or "h0 ly h4n dgr 3n4d 3!"
+
+    SQLALCH_DB_URI = os.environ.get("DATABASE_URL") \
+        or "sqlite:///" + os.path.join(base_dir, "data", "mypylib.db")
+
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,28 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+
+from app.LoginForm import LoginForm
 
 
 @app.route("/")
 def index():
     return render_template(
         "index.html"
+    )
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash("Login requested for user {}, remember = {}".format(form.email.data, form.remember.data))
+        return redirect(url_for("index"))
+
+    return render_template(
+        "login.html",
+        title="Log In",
+        form=form
     )
 
 
