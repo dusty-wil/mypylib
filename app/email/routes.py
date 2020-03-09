c911a7a95805f9a2d2622837d622881f81e452fe
Dusty Williams <dusty.wil@gmail.com>
2020-03-08 23:13:36 -0400

    email library functionality and added reqs - dmw

--- app/email/routes.py
+++ app/email/routes.py
@@ -1,14 +1,19 @@
+from app import app
 from app.email import bp
 from app.email.email import confirm_activation_token
 from app import db
 
 from flask import render_template, flash, redirect, url_for
-from flask_login import current_user
+from flask_login import current_user, login_required
 from datetime import datetime
 
 from app.models.User import User
-from app.email.email import send_activation_email
+from app.email.email import send_activation_email, send_mail
 from app.email.forms.ResendActivationForm import ResendActivationForm
+from app.email.forms.ShareLibraryForm import ShareLibraryForm
+
+from app.models.UserBooks import UserBooks
+from app.models.Book import Book
 
 
 @bp.route("/activate/<token>")
@@ -59,5 +64,41 @@ def resend_activate_email():
     return render_template(
         "email/resend_link.html",
         title="Resend Activation Link",
+        form=form
+    )
+
+
+@bp.route("/library/share", methods=["GET", "POST"])
+@login_required
+def share_library():
+    form = ShareLibraryForm()
+
+    if form.validate_on_submit():
+        lib = UserBooks.query.join(Book).join(User).filter_by(id=current_user.id).all()
+
+        send_mail(
+            subject="My Little Python Library: Shared Library",
+            sender=app.config['ADMIN'],
+            recipients=[form.email.data],
+            txt_body=render_template(
+                "email/share_library.txt",
+                lib=lib,
+                recipient=form.email.data,
+                user_email=current_user.email
+            ),
+            html_body=render_template(
+                "email/share_library.html",
+                lib=lib,
+                recipient=form.email.data,
+                user_email=current_user.email
+            )
+        )
+
+        flash("Your library has been shared with {}!".format(form.email.data))
+        return redirect(url_for("my_library.summary"))
+
+    return render_template(
+        "email/share_library_with.html",
+        title="Share Your Library",
         form=form
     )
